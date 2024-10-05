8345909f1f5cb596859035af50c3cfad4ed9b655
nunnyu <antony.basko@gmail.com>
2024-10-05 17:50:07 -0400

    Cat Movement
    Cat movement + Flip
    Mouse Flip
    Respawn Mechanic

---
+++ Assets/Scripts/CatMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CatMovement : MonoBehaviour {
+    public Transform target;
+    public float speed = 3f;
+
+    // Update is called once per frame
+    void Update() {
+        if ((target.transform.position.y) < (transform.position.y + 1f) & 
+            (target.transform.position.y) > (transform.position.y - 1f)) {
+                speed = 10f;
+                Debug.Log("silly speed");
+        } else {
+            speed = 3f;
+        }
+        
+        Vector3 scale = transform.localScale;
+        if ((target.transform.position.x) > (transform.position.x)) {
+            scale.x = 1;
+            transform.localScale = scale;
+        } else {
+            scale.x = -1;
+            transform.localScale = scale;
+        }
+
+
+        if (target != null) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, target.position, step);
+        }
+    }
+}
